8b3fc8a907054698d4fbc874cd11437b71628c80
Tsuyoshi Yoshioka <tsuyogoro@gmail.com>
2021-04-29 11:39:00 +0900

    https://rnfirebase.io/#2-android-setup and #3

--- App.tsx
+++ App.tsx
@@ -73,7 +73,7 @@
            style={{
              backgroundColor: isDarkMode ? Colors.black : Colors.white,
            }}>
-           <Section title="Step One">
+           <Section title="Step Oneeeee">
              Edit <Text style={styles.highlight}>App.js</Text> to change this
              screen and then come back to see your edits.
            </Section>
