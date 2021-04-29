f6e322b58d7ca5926ad7e5bac7f357491f445af4
Tsuyoshi Yoshioka <tsuyogoro@gmail.com>
2021-04-30 06:02:55 +0900

    Integrate navigation and make empty page

---
+++ src/app/screens/Login.tsx
@@ -0,0 +1,40 @@
+import * as React from 'react';
+
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+
+export const Login = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <Header />
+        <Text>Login Login Login</Text>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
